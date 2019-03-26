769a70325c75abd322e38fa5ccb3d27d4af7334a
cloudle <sonle@paperpile.com>
2019-03-27 03:28:55 +0700

    dev tool that automatically install capslock to karabiner..

---
+++ dev.js
@@ -0,0 +1,15 @@
+const chokidar = require('chokidar');
+const fs = require('fs');
+
+const watcher = chokidar.watch('./mac', { ignoreInitial: false });
+
+watcher.on('all', (event, filename) => {
+	console.log(filename);
+	if (filename === 'mac/capslock.json') {
+		const dest = '/Users/cloudle/.config/karabiner/assets/complex_modifications/capslock.json';
+		fs.copyFile('./mac/capslock.json', dest, (err) => {
+			if (err) console.log(err);
+			else console.log('successfully copy capslock.json to karabiner!')
+		});
+	}
+});
